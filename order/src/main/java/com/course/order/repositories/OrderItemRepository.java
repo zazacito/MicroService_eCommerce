0793d5a9b13bf122dfa2aa2f17898a131ed10299
zaza <61968508+zazacito@users.noreply.github.com>
2021-01-28 18:25:28 +0100

    Revert "Merge pull request #1 from zazacito/Microservice-Order-Branch"
    This reverts commit 97041756f12ffe52f59b1ae7ba01370050f5f37d, reversing
    changes made to a6d5875f2a3e4eb37250137dd72bfce9876504d2.

--- order/src/main/java/com/course/order/repositories/OrderItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.course.order.repositories;
-
-import com.course.order.domain.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<Order, Long> {
-}
